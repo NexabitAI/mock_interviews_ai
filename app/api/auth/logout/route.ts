3fc0d3e5dfd82d3942b6690827ee8e54b0ddb847
shamsk41 <shamsk.dev@gmail.com>
2026-02-18 22:23:39 +0500

    signout button fix

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  const res = NextResponse.json({ success: true });
-
+export async function POST(req: Request) {
+  const res = NextResponse.redirect(new URL("/sign-in", req.url), {
+    status: 303,
+  });
   res.cookies.set("session", "", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
     path: "/",
     maxAge: 0,
+    // domain: ".yourdomain.com", // include ONLY if you also set it at login
   });
-
   return res;
 }
